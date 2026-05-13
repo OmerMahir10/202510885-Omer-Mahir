02567fcc18567cb78d3e8557cd5ff17063e004e2
OmerMahir10 <omarmahir126@gmail.com>
2026-05-13 10:50:07 +0300

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -5,7 +5,7 @@ int main() {
     int arr[] = {109, 99, 23, 45, 23, 2, 5, 1};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    // Bubble Sort
+    
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
@@ -17,7 +17,7 @@ int main() {
         }
     }
 
-    // Display sorted array
+   
     cout << "Sorted numbers in ascending order: ";
     for (int i = 0; i < n; i++) {
         cout << arr[i];
